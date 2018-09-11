bcb94e6f92f1f15a2668b3a291af911f2f9299f6
Chong <35554978+tpg1chong@users.noreply.github.com>
2018-09-11 18:42:25 +0700

    New Project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+import './assets/css/main.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <div id="livechat" className="livechat">
+          <div className="root">
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
